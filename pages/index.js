c350457e31c2b184a7c0c34d41771ced5079f738
Clive Gordon <clive.gordon@nationwide.co.uk>
2022-01-30 16:23:35 +0000

    Fix build error by adding Link to index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import { useUser } from '@auth0/nextjs-auth0';
 
+import Link from 'next/link';
+
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
@@ -15,12 +17,19 @@ export default function Home() {
   if(user) {
     return (
       <div>
-        <h1>Hi {user.email}</h1>
-        <a href="/api/auth/logout">Log me out</a>
+        <h1>Hi {user.email} welcome to DIP</h1>
+        <Link href="/api/auth/logout">
+          <a>Logout</a>
+        </Link>
       </div>
     )
   }
 
-  return <a href="/api/auth/login">Login</a>
-
+  return (
+    <div>
+    <Link href="/api/auth/login">
+      <a>Login please</a>
+    </Link>
+  </div>
+  )
 }
