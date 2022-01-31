a45aaef7a3236142257e512f538c493f0f98eaff
Clive Gordon <clive.gordon@nationwide.co.uk>
2022-01-31 15:59:43 +0000

    Add _document page to add NBS fonts into <head>, convert from css to scss

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { UserProvider } from '@auth0/nextjs-auth0';
 
-import '../styles/globals.css'
+import '../styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/Home.module.scss'
 
 export default function Home() {
   return (
