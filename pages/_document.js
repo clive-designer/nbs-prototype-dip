23df71bc2e6bcaf3b7121a7a0c58f0800ed9e4d9
Clive Gordon <clive.gordon@nationwide.co.uk>
2022-02-02 21:25:21 +0000

    Add nbs fonts locally, fixes font not looked quite right, fix for log in icon being cut off

--- pages/_document.js
+++ pages/_document.js
@@ -10,14 +10,63 @@ class dipDocument extends Document {
     return (
       <Html>
         <Head>
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/CabernetJFPro-Regular.woff2" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/CabernetJFPro-Regular.woff" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Bold.woff2" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Bold.woff" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Light.woff2" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/assets/NBS-Light.woff" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Medium.woff2" rel="stylesheet" />
-          <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Medium.woff" rel="stylesheet" />
+        <link
+            rel="preload"
+            href="/assets/CabernetJFPro-Regular.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/assets/CabernetJFPro-Regular.woff"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/assets/NBS-Bold.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/assets/NBS-Bold.woff"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/assets/NBS-Light.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="/assets/NBS-Light.woff"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="assets/NBS-Medium.woff2"
+            as="font"
+            crossOrigin=""
+          />
+          <link
+            rel="preload"
+            href="assets/NBS-Medium.woff"
+            as="font"
+            crossOrigin=""
+          />
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/CabernetJFPro-Regular.woff2" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/CabernetJFPro-Regular.woff2" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/CabernetJFPro-Regular.woff" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Bold.woff2" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Bold.woff" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Light.woff2" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/assets/NBS-Light.woff" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Medium.woff2" rel="stylesheet" /> */}
+          {/* <link href="https://dip.mortgages.nationwide.co.uk/assets/NBS-Medium.woff" rel="stylesheet" /> */}
         </Head>
         <body>
           <Main />
